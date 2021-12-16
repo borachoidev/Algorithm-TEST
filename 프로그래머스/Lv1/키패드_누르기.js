976314cde2119b2babfc39566c68fd95e7786690
Bora Choi <66766189+september21st@users.noreply.github.com>
2021-12-16 14:38:25 +0900

    chore: 주석추가

--- 프로그래머스/Lv1/키패드_누르기.js
+++ 프로그래머스/Lv1/키패드_누르기.js
@@ -6,24 +6,31 @@ function solution(numbers, hand) {
 
   for (let i = 0; i < numbers.length; i++) {
     const key = numbers[i]
+    // 왼쪽키에 속하면 왼쪽을 누르고, 현재 손위치 변경
     if (leftKey.includes(key)) {
       answer += 'L'
       currentPosition[0] = key
+      // 오른쪽키에 속하면 오른쪽을 누르고, 현재 손위치 변경
     } else if (rightKey.includes(key)) {
       answer += 'R'
       currentPosition[1] = key
+      // 가운데 번호일 경우 최단거리 혹은 주사용손으로 누르고, 현재 손위치변경
     } else {
+      //최단 거리 구하기
       const shortPath = getShortPath(
         currentPosition[0],
         currentPosition[1],
         key
       )
+      //최단거리가 왼쪽일경우
       if (shortPath === 'L') {
         answer += 'L'
         currentPosition[0] = key
+        //최단거리가 오른쪽일 경우
       } else if (shortPath === 'R') {
         answer += 'R'
         currentPosition[1] = key
+        //최단거리가 같을경우 주상용손
       } else {
         answer += hand === 'right' ? 'R' : 'L'
         currentPosition[hand === 'right' ? 1 : 0] = key
@@ -33,8 +40,9 @@ function solution(numbers, hand) {
   return answer
 }
 
-const getShortPath = ( l, r, key) => {
-    const position = {
+const getShortPath = (l, r, key) => {
+  // 각 키패드별 좌표로 만들기
+  const position = {
     1: [0, 0],
     2: [0, 1],
     3: [0, 2],
@@ -48,6 +56,7 @@ const getShortPath = ( l, r, key) => {
     0: [3, 1],
     '#': [3, 2],
   }
+
   const leftPostion =
     Math.abs(position[l][0] - position[key][0]) +
     Math.abs(position[l][1] - position[key][1])
