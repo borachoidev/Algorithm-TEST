5b555a735ad98ea38e57f3ba9ee410a99a550ce3
borachoidev <tsuilook@gmail.com>
2022-04-14 22:52:49 +0900

    자료구조: 양방향 링크드리스트 insert메서드

--- data-structures/doubly-linked-list.js
+++ data-structures/doubly-linked-list.js
@@ -94,12 +94,28 @@ class DoublyLinkedList {
   }
 
   set(index, val) {
-    const founNode = this.get(index)
-    if (founNode !== null) {
-      founNode.val = val
+    const foundNode = this.get(index)
+    if (foundNode !== null) {
+      foundNode.val = val
       return true
     }
     return false
+  }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false
+    if (index === 0) return this.unshift(val)
+    if (index == this.length) return this.push(val)
+
+    const newNode = new Node(val)
+    const beforeNode = this.get(index - 1)
+    const afterNode = beforeNode.next
+    beforeNode.next = newNode
+    newNode.prev = beforeNode
+    newNode.next = afterNode
+    afterNode.prev = newNode
+    this.length++
+    return true
   }
 }
 
